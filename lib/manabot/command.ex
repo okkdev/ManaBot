7c6d8deb4719365ccf7e47596fac798827b58588
jen.stehlik <jen.stehlik@kallysoft.ch>
2019-09-12 10:12:23 +0200

    german language support

--- lib/manabot/command.ex
+++ lib/manabot/command.ex
@@ -1,4 +1,8 @@
 defmodule Manabot.Command do
+  @moduledoc """
+  The code in this module sucks and it hurts
+  """
+
   alias Nostrum.Api
   import Nostrum.Struct.Embed
 
@@ -12,8 +16,28 @@ defmodule Manabot.Command do
   Sunday:
   """
 
+  @tage """
+  Montag:
+  Dienstag:
+  Mittwoch:
+  Donnerstag:
+  Freitag:
+  Samstag:
+  Sonntag:
+  """
+
   @hours """
   Closed
+  16:00 - 00:00
+  16:00 - 00:00
+  16:00 - 00:00
+  16:00 - 02:00
+  10:00 - 02:00
+  10:00 - 22:00
+  """
+
+  @stunden """
+  Geschlossen
   16:00 - 00:00
   16:00 - 00:00
   16:00 - 00:00
@@ -28,26 +52,38 @@ defmodule Manabot.Command do
         Api.create_message(msg.channel_id, "pong")
 
       String.contains?(String.downcase(msg.content), "opening hours") ->
-        openinghours(msg)
+        openinghours(msg, "en")
 
       String.contains?(String.downcase(msg.content), "öffnungs") ->
-        openinghours(msg)
+        openinghours(msg, "de")
 
       String.contains?(String.downcase(msg.content), "öffnigs") ->
-        openinghours(msg)
+        openinghours(msg, "de")
 
       true ->
         :ignore
     end
   end
 
-  def openinghours(msg) do
-    embedmsg =
-      %Nostrum.Struct.Embed{}
-      |> put_title("Opening hours")
-      |> put_field("Days", @days, true)
-      |> put_field("Hours", @hours, true)
+  def openinghours(msg, lang) do
+    case lang do
+      "en" ->
+        embedmsg =
+          %Nostrum.Struct.Embed{}
+          |> put_title("Opening hours")
+          |> put_field("Days", @days, true)
+          |> put_field("Hours", @hours, true)
 
-    Api.create_message(msg.channel_id, embed: embedmsg)
+        Api.create_message(msg.channel_id, embed: embedmsg)
+
+      "de" ->
+        embedmsg =
+          %Nostrum.Struct.Embed{}
+          |> put_title("Öffnungszeiten")
+          |> put_field("Tage", @tage, true)
+          |> put_field("Zeiten", @stunden, true)
+
+        Api.create_message(msg.channel_id, embed: embedmsg)
+    end
   end
 end
